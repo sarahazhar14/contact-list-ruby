7b8db4ac46301c1494833a4643e77f0f5b3acfee
sarahazhar14 <156925002+sarahazhar14@users.noreply.github.com>
2024-03-28 00:53:43 +0500

    ruby-contact-list

--- Contacts.rb
+++
@@ -1,7 +0,0 @@
-class Contact 
-    def initialize (name, phone_number, email_address)
-        @name = name 
-        @phone_number = phone_number 
-        @email_address = email_address 
-    end 
-end 
---
+++ contact.rb
@@ -0,0 +1,8 @@
+class 'contact'
+    def initialize(name, phone_number, email_address)
+        @name = name
+        @phone_number = phone_number
+        @email_address = email_address
+    end 
+
+end
